39b122c6539e8222f926243dcc54314b5713f60a
Ksenija Lakotko <166509373+23DP3KLako@users.noreply.github.com>
2024-11-15 12:12:22 +0000

    Statistics(4part)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,27 +6,7 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ){
-        String[] words = {"sebastian", "lucas", "hanna", "garbiel"};
-        String[] years = {"2017", "2017", "2017", "2014", "2009"};
-
-        int sum = 0;
-        for (String year : years) {
-            sum += Integer.parseInt(year);
-        }
-        int averageYear = (int) sum / years.length;
-
-        String longestWord = "";
-        int maxLength = 0;
-
-        for (String word : words) {
-            if (word.length() > maxLength) {
-                longestWord = word;
-                maxLength = word.length();
-
-            }
-        }
-        System.out.println("The longest name: " + longestWord);
-        System.out.println("Average of the birth years: " + averageYear);
+        
     }
 
 
