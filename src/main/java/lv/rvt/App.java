fe5c045828a498358730fc0ced2ebca368e584a1
Ksenija Lakotko <166509373+23DP3KLako@users.noreply.github.com>
2024-12-06 07:00:53 +0000

    git fix

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,12 +1,16 @@
 package lv.rvt;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+
 
 public class App 
 {
     public static void main( String[] args ){
-        
+        Person paul = new Person("Paul", 24);
+        Person ada = new Person("Ada");
+
+        System.out.println(paul);
+        System.out.println(ada);
     }
 
 
